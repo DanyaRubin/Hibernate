f973dc2862b1e4b4c30a225353f55c7e99ed4734
DanyaRubin <masterovski@gmail.com>
2021-12-09 08:00:29 +0400

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    Connection connection;
+
+    {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/test", "root",
+                    "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
