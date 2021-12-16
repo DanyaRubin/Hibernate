48bcf6a51e559ce7a4675e912e3625e6efceab40
DanyaRubin <masterovski@gmail.com>
2021-12-16 15:18:49 +0400

    final

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,30 +5,30 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-         final UserService userService = new UserServiceImpl();
+        final UserService userService = new UserServiceImpl();
 
-         final String testName1 = "Ivan";
-         final String testLastName1 = "Petrov";
-         final byte testAge1 = 5;
+        final String testName1 = "Ivan";
+        final String testLastName1 = "Petrov";
+        final byte testAge1 = 5;
 
-         final String testName2 = "Petr";
-         final String testLastName2 = "Ivanov";
-         final byte testAge2 = 6;
+        final String testName2 = "Petr";
+        final String testLastName2 = "Ivanov";
+        final byte testAge2 = 6;
 
-         final String testName3 = "Vasily";
-         final String testLastName3 = "Utkin";
-         final byte testAge3 = 7;
+        final String testName3 = "Vasily";
+        final String testLastName3 = "Utkin";
+        final byte testAge3 = 7;
 
-         final String testName4 = "Utka";
-         final String testLastName4 = "Vasilieva";
-         final byte testAge4 = 8;
+        final String testName4 = "Utka";
+        final String testLastName4 = "Vasilieva";
+        final byte testAge4 = 8;
 
         try {
             userService.createUsersTable();
 
             userService.saveUser(testName1,testLastName1, testAge1);
             System.out.println("User с именем " + testName1 + " добавлен в базу данных");
-/*
+
             userService.saveUser(testName2,testLastName2, testAge2);
             System.out.println("User с именем " + testName2 + " добавлен в базу данных");
 
@@ -42,8 +42,6 @@ public class Main {
 
             userService.cleanUsersTable();
             userService.dropUsersTable();
-
- */
         } catch (Exception e) {
             System.out.println("При тестировании создания таблицы пользователей произошло исключение\n" + e.getMessage());
         }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,48 +5,41 @@ import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
-
-//import javax.persistence.EntityManager;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-
-    // private EntityManager em;
 
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-
-        String sql = "CREATE TABLE IF NOT EXISTS users " +
-                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-                "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
-                "age INT NOT NULL)";
-
-        Query query = session.createSQLQuery(sql).addEntity(User.class);
-        query.executeUpdate();
-
-        transaction.commit();
-        session.close();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            String sql = "CREATE TABLE IF NOT EXISTS users " +
+                    "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                    "age INT NOT NULL)";
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
+        try(Session session = Util.getSessionFactory().openSession()){
         Transaction transaction = session.beginTransaction();
-
         String sql = "DROP TABLE IF EXISTS users";
-
         Query query = session.createSQLQuery(sql).addEntity(User.class);
         query.executeUpdate();
-
         transaction.commit();
-        session.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -57,30 +50,52 @@ public class UserDaoHibernateImpl implements UserDao {
             User user = new User(name, lastName, age);
             session.beginTransaction();
             session.save(user);
-            session.beginTransaction().commit();
+            session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-        @Override
-        public void removeUserById ( long id){
-            Session session = Util.getSessionFactory().openSession();
+    @Override
+    public void removeUserById(long id) {
+            /* Session session = Util.getSessionFactory().openSession();
             Transaction transaction = session.beginTransaction();
             Query query = session.createQuery("delete User where id = :id");
             query.setParameter("id", id);
             transaction.commit();
             query.executeUpdate();
+            */
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        @Override
-        public List<User> getAllUsers () {
-            return null;
+    @Override
+    public List<User> getAllUsers() {
+        List<User> users = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            users = (List<User>) session.getSessionFactory().openSession()
+                    .createQuery("From User").list();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return users;
+    }
+
 
         @Override
         public void cleanUsersTable () {
-
+            try (Session session = Util.getSessionFactory().openSession()) {
+                session.beginTransaction();
+                session.createQuery("delete from User").executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 }
 
